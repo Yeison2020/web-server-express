cb6013af27558326bb8a8e07afb74d191c068fb4
Yeison <enmanuelquiterios@gmail.com>
2022-03-20 13:09:20 -0400

    Adding customs routes

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
 const express = require("express");
-
 const app = express();
+const port = 8080;
 
-app.get("/", function (req, res) {
+// Body of my request
+
+app.get("/", (req, res) => {
   res.send("Home page here");
 });
 
-app.get("/hello-mundo", function (req, res) {
+app.get("/hello-mundo", (req, res) => {
   res.send("Hello World Working with express");
 });
 
 // Any routes that is not define here will be send to this route
+// Error pages how to handle them
 
-app.get("*", function (req, res) {
+app.get("*", (req, res) => {
   res.send("Sorry Route still being built");
 });
 console.log("Listening in port ", 8080);
 
-app.listen(8080);
+// Best way to change the port running in one place
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});

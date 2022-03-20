ae9ff95273b7b9dbe3ba58a854f4d6ca0fe3ab32
Yeison <enmanuelquiterios@gmail.com>
2022-03-20 13:04:04 -0400

    Adding default

--- app.js
+++ app.js
@@ -2,8 +2,18 @@ const express = require("express");
 
 const app = express();
 
-app.get((req, res) => {
+app.get("/", function (req, res) {
+  res.send("Home page here");
+});
+
+app.get("/hello-mundo", function (req, res) {
   res.send("Hello World Working with express");
+});
+
+// Any routes that is not define here will be send to this route
+
+app.get("*", function (req, res) {
+  res.send("Sorry Route still being built");
 });
 console.log("Listening in port ", 8080);
 
